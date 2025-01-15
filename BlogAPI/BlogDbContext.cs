7d4c598bf4a548f7e696305e66480a5107bcdff1
okayo <okayo@SAMSEPI0L>
2025-01-15 07:39:25 +0300

    Add project files.

---
+++ BlogAPI/BlogDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BlogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAPI
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<DbContext> options) : base(options) { }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Post>(entity => 
+            { 
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Title).IsRequired().HasMaxLength(100);
+                entity.Property(p => p.Content).IsRequired();
+                entity.Property(p => p.Author).IsRequired().HasMaxLength(50);
+                entity.Property(p => p.UpdatedDate).IsRequired();
+            });
+        }
+    }
+}
---
+++ BlogAPI/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogAPI.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set;}
+    }
+}
